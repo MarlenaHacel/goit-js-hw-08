a2b5fd040d145c24ae1cb2e762e9e8ab713b5e37
MarlenaHacel <marlenahacel@wp.pl>
2023-08-30 18:37:17 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,14 +4,9 @@ const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('input');
 const messageInput = document.querySelector('textarea');
 
-const saveForm = throttle(() => {
-  const textInput = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(textInput));
-}, 500);
-
+document.addEventListener('DOMContentLoaded', () => {
+  loadForm();
+});
 const loadForm = () => {
   const storedForm = localStorage.getItem('feedback-form-state');
   if (storedForm) {
@@ -20,6 +15,14 @@ const loadForm = () => {
     messageInput.value = parsedForm.message;
   }
 };
+
+const saveForm = throttle(() => {
+  const textInput = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(textInput));
+}, 500);
 
 emailInput.addEventListener('input', saveForm);
 messageInput.addEventListener('input', saveForm);
